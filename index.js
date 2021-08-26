758dd908514b4ae0e322e18f80e2208656598947
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-08-26 21:34:58 +0200

    Added basic code and dependencies

---
+++ index.js
@@ -0,0 +1,13 @@
+const glob = require('glob');
+function recursiveOperation(selector,action){
+    ret = [];
+glob(selector,null,function(er,files){
+    if(er!=undefined){
+        ret[0]=er;
+    }
+    for(file in files){
+        ret.push(action(files));
+    }
+})
+}
+export default recursiveOperation;
